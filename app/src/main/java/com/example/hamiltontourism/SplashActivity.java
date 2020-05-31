9249b933ebd4b376f965911096c8495c6a5fa8ea
mechanicRyan <53892626+mechanicRyan@users.noreply.github.com>
2020-06-01 02:22:53 +1000

    Initial commit

---
+++ app/src/main/java/com/example/hamiltontourism/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.hamiltontourism;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+public class SplashActivity extends AppCompatActivity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+
+		new Handler().postDelayed(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				Toast.makeText(getBaseContext(), "Splash OK", Toast.LENGTH_LONG).show();
+			}
+		}, 3000);
+	}
+}
